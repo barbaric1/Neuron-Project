621cd5ee5446bf58bf7be52ce6ada60eb7c08c97
Barbaric Robert <barbaric@IC-CO-IN-SC134.intranet.epfl.ch>
2017-10-27 13:31:46 +0200

    version 0

---
+++ Build/CMakeFiles/neuron.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/INTRANET/barbaric/Desktop/myfiles/Neuron Program/src/main.cpp" "/home/INTRANET/barbaric/Desktop/myfiles/Neuron Program/Build/CMakeFiles/neuron.dir/main.cpp.o"
+  "/home/INTRANET/barbaric/Desktop/myfiles/Neuron Program/src/neuron.cpp" "/home/INTRANET/barbaric/Desktop/myfiles/Neuron Program/Build/CMakeFiles/neuron.dir/neuron.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ src/neuron.hpp
@@ -0,0 +1,15 @@
+#ifndef NEURON_H
+#define NEURON_H
+
+
+class Neuron {
+	
+	
+	
+	
+};
+
+
+
+
+#endif
