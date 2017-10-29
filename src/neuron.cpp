e2631667ff70ca61de7b76e95b86154df1d8663a
Barbaric Robert <barbaric@SVBBCF20170045.intranet.epfl.ch>
2017-10-29 15:28:23 +0100

    Version 2.0

--- src/neuron.cpp
+++ src/neuron.cpp
@@ -9,35 +9,70 @@ const double MEMBRANE_TIME_CONSTANT_TAU = 20;
 const double MEMBRANE_RESISTENCE = 20 //Questo dovra cambiare dopo//
 
 
-vector<unsigned int> Neuron::update(unsigned int stopTime)
+void Neuron::update(unsigned int stopTime, double curr)
 {
+	//FORSE QUA CI VORRA UN SETTER PER SETTARE neuronIsRefractory A FALSE INIZIALMENTE
+	
+	setCurrent(curr);
+	
 	do
-	{ if(membranePotential > STANDARD_MEMBRANE_POT)
+	{ if(getMembranePot() > STANDARD_MEMBRANE_POT)
 		{
-			spikes.push_back(neuronTime);
+			spikesTime.push_back(neuronTime);
 			neuronIsRefractory = true; //qu8a potrebbe servire un setter//
 		}
 		
-		else if(neuronIsRefractory == true) //forse devo metterlo prima del primo "if", o piu probabilmente neuron is refractory non serve neanche e mi basta nella prima boucle if settare membranepotential a 0//
+		else if((neuronIsRefractory == true) and (getMembranePot() != 0)) //forse devo metterlo prima del primo "if", o piu probabilmente neuron is refractory non serve neanche e mi basta nella prima boucle if settare membranepotential a 0//
 		{
-			membranePotential = 0; //forse anche qui setter//
+			setMembranePot(0); //forse anche qui setter//
 		}
 		
 		else
 		{
-			membranePotential = (exp((-1)*(REFRACTORY_TIME/MEMBRANE_TIME_CONSTANT_TAU))* membranePotential) + current * MEMBRANE_RESISTENCE * (1 - exp((-1)*(REFRACTORY_TIME/MEMBRANE_TIME_CONSTANT_TAU)));  //qua bisognera aggiungere J e inoltre potrebbe volerci l'utilizzo di getter e setter. Anche R dovra cambiare molto probabilmente
+			setMembranePot((exp((-1)*(REFRACTORY_TIME/MEMBRANE_TIME_CONSTANT_TAU))* getMembranePot()) + (getCurrent() * MEMBRANE_RESISTENCE * (1 - exp((-1)*(REFRACTORY_TIME/MEMBRANE_TIME_CONSTANT_TAU)))));  //qua bisognera aggiungere J e inoltre potrebbe volerci l'utilizzo di getter e setter. Anche R dovra cambiare molto probabilmente
 			neuronIsRefractory = false;
 		}
-		
+		spikesMembranePot.push_back(getMembranePot());
 		neuronTime += REFRACTORY_TIME;
 		
 		
 	} while (neuronTime < stopTime);
-	
-	return spikes;
-	
 }
 
+void Neuron::setMembranePot(double m)
+{
+	membranePotential = m;
+}
+
+double Neuron::getMembranePot()
+{
+	return membranePotential;
+}
+
+unsigned int Neuron::getNumberOfSpikes()
+{
+	return spikesTime.size();
+}
+
+void Neuron::storeSpikesMembrane&Time() 
+{
+	cout << "Membrane Potential ********** Time(ms)" << endl;
+	
+	for(size_t i(0); i < spikesTime.size(); i++)
+	{
+		cout << spikesMembranePot[i] << " ********** " << spikesTime[i] << endl;
+	}
+}
+	
+void Neuron::setCurrent(double c)
+{
+	current = c;
+}
+
+double Neuron::getCurrent()
+{
+	return current;
+}
 
 		
 		
--- src/neuron.hpp
+++ src/neuron.hpp
@@ -7,15 +7,22 @@ class Neuron {
 	
 	private:
 	
-	std::vector<unsigned int> spikes;
+	std::vector<unsigned int> spikesTime;
 	std::double membranePotential;
 	std::unsigned int neuronTime;
 	std::double current;
 	std::bool neuronIsRefractory;
+	std::vector<double> spikesMembranePot;
 	
 	public:
 	
-	std::vector<unsigned int> update(unsigned int stopTime);
+	std::void update(unsigned int stopTime, double curr);
+	std::void setMembranePot(double m);
+	std::double getMembranePot();
+	std::unsigned int getNumberOfSpikes();
+	std::void storeSpikesMembrane&Time();
+	std::void setCurrent(double c);
+	std::double getCurrent();
 	
 	
 	
